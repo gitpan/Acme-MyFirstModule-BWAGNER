3592e7496311f6356b8a29b3ba6037ea00e5a7f1
Bernhard Wagner <bwagner@cpan.org>
2014-09-06 09:50:25 +0000

    Import of BWAGNER/Acme-MyFirstModule-BWAGNER-0.03 from CPAN.
    gitpan-cpan-distribution: Acme-MyFirstModule-BWAGNER
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         BWAGNER/Acme-MyFirstModule-BWAGNER-0.03.tar.gz
    gitpan-cpan-author:       BWAGNER
    gitpan-cpan-maturity:     released

--- lib/Acme/MyFirstModule/BWAGNER.pm
+++ lib/Acme/MyFirstModule/BWAGNER.pm
@@ -10,11 +10,11 @@ Acme::MyFirstModule::BWAGNER - The great new Acme::MyFirstModule::BWAGNER!
 
 =head1 VERSION
 
-Version 0.01
+Version 0.03
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 
 =head1 SYNOPSIS
@@ -39,7 +39,7 @@ if you don't export anything, such as for a purely object-oriented module.
 
 =cut
 
-subfunction1 {
+sub function1 {
 }
 
 =head2 function2
